0942e1d81d7c8f26ed39ac473b524c77959907c7
Dart <rahmatulla.r08@gmail.com>
2025-12-28 03:30:18 +0300

    Delete DatabaseContext/DatabaseContext directory

--- DatabaseContext/DatabaseContext/Context/DbContextFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
-
-namespace DatabaseContext.Context
-{
-    public class UnivContextFactory : IDesignTimeDbContextFactory<UnivContext>
-    {
-        public UnivContext CreateDbContext(string[] args) => new(DbContextOptionsFactory.Create<UnivContext>());
-    }
-}
